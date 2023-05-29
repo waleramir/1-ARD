b9f3877fd52af0523b99f424ab484a855c113c10
waleramir <42164244+waleramir@users.noreply.github.com>
2023-05-29 08:02:28 +0300

    Update 1_task_2.ino

--- 1_task_2/1_task_2.ino
+++ 1_task_2/1_task_2.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+int button = 5;
+int led = 9;
+volatile bool Flag = 0;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+  attachInterrupt(digitalPinToInterrupt(button), Button, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(Flag);
+  digitalWrite(led, Flag);
+  delay(50);
+}
 
+void Button() {
+  Flag = !Flag;
 }
